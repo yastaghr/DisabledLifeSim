1f41d7fb023308bad422dba643d23174cf869b79
Liana Bardallis <yastaghr@gmail.com>
2020-05-06 10:47:15 -0600

    basic outline 2
    more in depth (and accurate) outline for playing the game

--- main.java
+++ main.java
@@ -1,37 +1,51 @@
 /* Author: Liana Bardallis
-* Date: 17 Mar, 2020
+* Date: 6 MAY 2020
 * Description: The main java file to run the Disabled Life Simulator game.
 */
 
 public class DisabledLifeSim{
-  private static void setup(){ // sets up the game to a general starting setup NOT WRITTEN
+  // sets up the game to a general starting setup NOT WRITTEN
+  private static Board setup(){
+    return new Board;
+  }
+
+  //plays the playGame NOT WRITTEN
+  private static void playGame(Board gameBoard){
     return;
   }
-  
-  private static boolean playGame(){
-    System.out.println("Do you want to continue? yes or no /n"); //asks if wants to continue
-    String choiceOfContinue = "no"; //get yes/no answer to continue question NOT WRITTEN
-    
+
+  //does the user want to continue?
+  private static boolean wantsToPlayGame(){
+    //asks if wants to continue
+    System.out.println("Do you want to continue? yes or no /n");
+    String choiceOfContinue = "no";
+
     //conditional for exit
-    if (choiceOfContinue.equals("yes"){ 
+    if (choiceOfContinue.equals("yes"){
       return true;
     } else {
       return false;
     }
   }
-  
-  private static void clearBoard(){ // clears board status of game NOT WRITTEN
+
+  // clears board status of game NOT WRITTEN
+  private static void clearBoard(){
     return;
   }
 
   public static void main(String[] args){
-    setup(); // sets up the game to a general starting setup
-    
+    Board gameBoard = setup();
+
     boolean continue = true;
     while (continue){
-      continue = playGame(); // plays the game, returns an int if player wants to play again. 
-      clearBoard(); // clears board status of game
+      // plays the game, returns bool if player wants to play again.
+      playGame(gameBoard);
+      continue = wantsToPlayGame();
+      clearBoard(gameBoard);
     }
-    System.out.println("Thank you for playing!"); //prints game over message
+
+    //end game
+    System.out.println("Thank you for playing!");
+    System.exit(1);
   }
 }
