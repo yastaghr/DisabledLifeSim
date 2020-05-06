045c468bc3242b38344546aca262fb4f5214bbfe
Liana Bardallis <yastaghr@gmail.com>
2020-05-06 11:51:27 -0600

    create Home.java and add location to Job
    Created Home's basic outline and added location value to Job

---
+++ Home.java
@@ -0,0 +1,27 @@
+/*Author: Liana Bardallis
+* Date: 6 May 2020
+* Description: Home object for DisabledLifeSim game
+*/
+
+class Home{
+  //values of home
+  private int spoonsToll, tax, cost;
+  private int[] location;
+  private String name;
+
+  //constants for no home
+  private static final String NO_HOME_NAME = "None";
+  private static final int NO_HOME_SPOONSTOLL = 0;
+  private static final int NO_HOME_TAX = 0;
+  private static final int NO_HOME_COST = 0;
+  private static final int[] NO_HOME_LOCATION = [0,0];
+
+  //creates new disability with no mods TODO
+  public Home(){
+    spoonsMod = NO_HOME_SPOONSTOLL;
+    name = NO_HOME_NAME;
+    tax = NO_HOME_TAX;
+    cost = NO_HOME_COST;
+    location = NO_HOME_LOCATION;
+  }
+}
--- Job.java
+++ Job.java
@@ -5,7 +5,7 @@
 
 class Job{
   //values of Job
-  private int speedToll, dextToll, spoonsToll;
+  private int speedToll, dextToll, spoonsToll, location;
   private String name;
 
   //constants for no job
@@ -13,12 +13,14 @@ class Job{
   private static final int NO_JOB_SPEEDTOLL = 0;
   private static final int NO_JOB_DEXTTOLL = 0;
   private static final int NO_JOB_SPOONSTOLL = 0;
+  private static final int NO_JOB_LOCATION = 0;
 
   //creates new disability with no mods TODO
-  public Disability(){
+  public Job(){
     speedToll = NO_JOB_SPEEDTOLL;
     dextToll = NO_JOB_DEXTTOLL;
     spoonsToll = NO_JOB_SPOONSTOLL;
     name = NO_JOB_NAME;
+    location = NO_JOB_LOCATION;
   }
 }
